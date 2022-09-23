d46d7d19f3ec235c1785ba00b7d20d8e1ae8ccb2
Ajay <arayansaini1999@gmail.com>
2022-09-24 03:25:10 +0530

    first commit

---
+++ mysite/mysite/views.py
@@ -0,0 +1,101 @@
+from fpdf import FPDF
+import webbrowser
+
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html') 
+
+def analyze(request):
+    #Get the Text ...........................
+    djtext=request.GET.get('text','default')
+    #........................................
+    
+    #Check remove Punc .........
+    removepunc = request.GET.get('removepunc','default')
+    capon = request.GET.get('capon','default')
+    removespace = request.GET.get('removespace','default')
+    pdff= request.GET.get('pdf','default')
+    removeline= request.GET.get('removeline','default')
+    analyzed=djtext
+    #........................................
+    punctuations='''!()-[]{}:;'"\,.<>/?@#$%^&*`~|'''
+    if removepunc=='on':
+        analyzed=""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed=analyzed+char
+    if capon=='on':
+        analyzed=analyzed.upper()
+        
+        
+    if removeline=='on':
+        for char in analyzed:
+            if char=='\n':
+                analyzed=analyzed.replace("\n","")
+                
+    
+    if removespace=='on':
+     
+        for char in analyzed:
+            if char==" ":
+                analyzed=analyzed.replace(" ",'')
+            elif char=='\n':
+                analyzed=analyzed.replace("\n",'')
+            elif char=="\t":
+                analyzed=analyzed.replace("\t",'')
+                # \t\n\r\t
+            elif char=='\r':
+                analyzed=analyzed.replace("\r",'')
+                
+                
+                
+                
+        # analyzed=analyzed.split()
+        
+    
+    
+                
+                
+    
+    #PDF...................................
+    
+# Python program to create
+# a pdf file
+ 
+ 
+ 
+ 
+    # save FPDF() class into a
+    # variable pdf
+    if pdff== 'on':
+        pdf = FPDF()
+        
+        # Add a page
+        pdf.add_page()
+        
+        # set style and size of font
+        # that you want in the pdf
+        pdf.set_font("Arial", size = 15)
+        
+        
+        
+        # add another cell
+        pdf.cell(300, 30, txt = analyzed,
+                ln = 100, align = 'C')
+        
+        # save the pdf with name .pdf
+        pdf.output("GFG.pdf") 
+        path='file:///D:/Django_harry/mysite/GFG.pdf'
+
+        webbrowser.open_new(path)
+        
+        
+        
+    params={'purpose': 'Remove Punctuations','analyzed_text': analyzed}
+    return render(request, 'analyze.html',params)
+
+
